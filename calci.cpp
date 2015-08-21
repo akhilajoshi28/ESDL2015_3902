05803112850b418f8d18983c59108623dd87c520
akhilajoshi28 <akhilasjoshi@gmail.com>
2015-08-21 14:43:38 +0530

    Edited again.

--- calci.cpp
+++ calci.cpp
@@ -1,4 +1,3 @@
 This is a calculator. Numerous mathematical operations can be performed on it.
 The various operaions the basic algebric operations. For example, addition, subtraction etc.
 Triginometric and log operations can be performed too.
-Huge calculations can be done in a few seconds.

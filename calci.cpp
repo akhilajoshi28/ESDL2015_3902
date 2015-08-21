82968545f522360c8673e6dbb2a8607143cbb6fe
akhilajoshi28 <akhilasjoshi@gmail.com>
2015-08-21 14:32:46 +0530

    This is my first file.

---
+++ calci.cpp
@@ -0,0 +1,3 @@
+This is a calculator. Numerous mathematical operations can be performed on it.
+The various operaions are adition, subtraction, multiplication and division.
+Triginometric and log operations can be performed too.

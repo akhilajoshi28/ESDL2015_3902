1c594c96632e8440dadbf1d9efe81da430d0c00c
akhilajoshi28 <akhilasjoshi@gmail.com>
2015-08-21 14:45:51 +0530

    branch

--- calci.cpp
+++ calci.cpp
@@ -1,3 +1,4 @@
 This is a calculator. Numerous mathematical operations can be performed on it.
 The various operaions the basic algebric operations. For example, addition, subtraction etc.
 Triginometric and log operations can be performed too.
+branch file

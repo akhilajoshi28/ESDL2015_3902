d5f47c212a787745cf12743adc98912f8be2a209
akhilajoshi28 <akhilasjoshi@gmail.com>
2015-08-21 14:35:17 +0530

    I have updated my first file.

--- calci.cpp
+++ calci.cpp
@@ -1,3 +1,4 @@
 This is a calculator. Numerous mathematical operations can be performed on it.
-The various operaions are adition, subtraction, multiplication and division.
+The various operaions the basic algebric operations. For example, addition, subtraction etc.
 Triginometric and log operations can be performed too.
+Huge calculations can be done in a few seconds.
